223a255592ce3bc197f11a49a54a0394a0c0714e
HongSeongJo <164273757+HongSeongJo@users.noreply.github.com>
2025-12-09 16:12:48 +0900

    probelm10926

--- README.md
+++ README.md
@@ -4,6 +4,7 @@
 - 2557 - Hello World(https://www.acmicpc.net/problem/2557)
 - 1000 - A + B(https://www.acmicpc.net/problem/1000)
 - 10869 - 사칙연산(https://www.acmicpc.net/problem/10869)
+- 10926 - ??!(https://www.acmicpc.net/problem/10926)
 ### 실버
 
 
---
+++ src/bronze/Problem10926.java
@@ -0,0 +1,12 @@
+package bronze;
+
+import java.util.*;
+
+public class Problem10926 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        String s= input.nextLine();
+
+        System.out.println(s+"??!");
+    }
+}
